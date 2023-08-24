5f5811537e5ced2f7e88667ccdf865c152a12277
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-08-24 08:18:47 +0100

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import Head from "next/head";
 import Lorem50 from "./Lorem50";
 import Profile from "./profile";
+import { motion } from "framer-motion";
 
 import {
   AiFillTwitterCircle,
@@ -25,27 +26,64 @@ import Scroll from "./scroll";
 import Testimonial from "./testimonial";
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
+  const variants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 50 },
+    tap: {
+      scale: 0.9,
+      transition: {
+        duration: 1,
+      },
+    },
+    hover: { scale: 1.2 },
+  };
+  const variantg = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  };
+
   return (
-    <div className={darkMode ? "dark" : ""}>
+    <motion.div
+      className={darkMode ? "dark" : ""}
+     
+    >
       <Head>
         <title>Chibuikem</title>
         <meta name="description" content="ggggg" />
       </Head>
-      <main className=" relative bg-white  text-black  dark:bg-gray-950">
+      <main className="selection:bg-green-200 selection:text-black dark:selection:bg-green-800  dark:selection:text-white relative bg-white  text-black  dark:bg-gray-950">
         <Scroll />
         <div className=" px-10 md:px-20 lg:px-40">
           <section className=" min-h-screen ">
-            <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+            <motion.div
+              variants={variantg}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 1 }}
+            >
+              <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+            </motion.div>
 
-            <Profile />
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
-            </div>
-            <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-10 overflow-hidden md:h-96 md:w-96">
+            <motion.div
+              variants={variants}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 2 }}
+             
+            >
+              <Profile />
+              <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+                <AiFillTwitterCircle />
+                <AiFillLinkedin />
+                <AiFillYoutube />
+              </div>
+            </motion.div>
+            <motion.div
+            
+              className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-10 overflow-hidden md:h-96 md:w-96"
+            >
               <Image src={deved} layout="fill" objectFit="cover" />
-            </div>
+            </motion.div>
           </section>
           <section>
             <div className="text-center">
@@ -157,6 +195,6 @@ export default function Home() {
           </section>
         </div>
       </main>
-    </div>
+    </motion.div>
   );
 }
